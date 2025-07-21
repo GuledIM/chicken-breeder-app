18a77783e2c850a95af16e92b8075ca109019f0d
GuledIM <guledmacallin937@gmail.com>
2025-07-21 19:13:04 +0100

    Completed migration to MySQL

---
+++ Week-4/src/app.py
@@ -0,0 +1,145 @@
+import time
+from functions import *
+
+def main():
+
+    manager = ChickenManager()
+
+    while True: 
+        
+        print("\n\tMain Menu:")
+        print("0 - Exit App")
+        print("1 - Print List of Chicken Records")
+        print("2 - Create New Chicken Record")
+        print("3 - Update Existing Chicken Record")
+        print("4 - Delete a Chicken Record")
+
+
+        x = input("Please select an option: \n")
+
+        if x.isdigit(): # again ensures the user can onlu enter numbers
+            x = int(x)
+            if x == 0:
+                print("Closing down...")
+                conn.close()
+                cursor.close()
+                exit() #exit the loop after printing closing down
+                time.sleep(2) #delay the clearing of the terminal
+                clr_terminal()
+            elif x == 1:
+                manager.print_all_chickens()
+            elif x == 2:
+                manager.add_chicken()
+            elif x == 3:
+                manager.update_chicken()
+            elif x == 4:
+                manager.delete_chicken()
+        else:
+            print("Invalid Input. Enter an integer like 1 or 2.")
+
+
+
+class  Chicken:
+    def __init__(self, id, name, breed, age):
+        self.id = id #added Id to be able to edit 
+        self.name = name
+        self.breed = breed
+        self.age = age
+    
+
+class ChickenManager:
+    def __init__(self):
+        self.next_id = 1 #Simple unique identifier for new entries, might implemet GUID in the future with hashing
+
+    
+    def print_all_chickens(self):
+
+        clr_terminal()
+
+        cursor.execute("SELECT * FROM chickens")
+
+        for row in cursor.fetchall():
+            print(row)
+    
+    def add_chicken(self):
+            
+            clr_terminal()
+            
+            if confirmation("Are you sure you would like to continue to add a new entry?") == 2:
+             time.sleep(3)
+             clr_terminal()
+             return 
+            
+            clr_terminal()
+                
+            name = string_input_validation("Enter the name of the Chicken:\n")
+            breed = string_input_validation("Enter the breed of this Chicken:\n")
+            age = integer_input_validation("Enter the age of the chicken in weeks:\n")
+
+            Chicken(self.next_id, name, breed, age) 
+
+            cursor.execute("INSERT INTO chickens (id, name, breed, age)" \
+            "VALUES (%s, %s, %s, %s)", 
+            (self.next_id, name, breed, age))
+
+            conn.commit()
+
+            self.next_id += 1 # +1 in preparation for next entry 
+
+            print("Chicken record created.") # Confirmation msg
+
+    
+    def update_chicken(self):
+
+        clr_terminal()
+
+
+        if confirmation("Are you sure you would like to update a record?") == 2:
+            time.sleep(3)
+            clr_terminal()
+            return
+
+        clr_terminal()
+        
+        ChickenManager.print_all_chickens(self)
+
+        chicken_id = user_ID_selection_validation('chickens',"Enter ID of chicken to update: ") #User selects ID of Chicken to edit
+
+        chicken_name = string_input_validation("Enter new name: ")
+
+        chicken_breed = string_input_validation("Enter new breed: ")
+
+        chicken_age = integer_input_validation("Enter new age (weeks): ")
+        
+        cursor.execute(f"UPDATE chickens SET name = '{chicken_name}', breed = '{chicken_breed}', age = '{chicken_age}' WHERE id = {chicken_id}")
+        
+        print("Chicken record updated.")
+        return
+        
+    
+    
+    def delete_chicken(self):
+
+        clr_terminal()
+
+        if confirmation("Are you sure you would like to delete a record?") == 2:
+            time.sleep(3)
+            clr_terminal()
+            return
+        
+        clr_terminal()
+
+        ChickenManager.print_all_chickens(self)
+
+        selected_id = user_ID_selection_validation('chickens',"Enter ID of the chicken you would like to delete:\n")
+
+        cursor.execute(f"DELETE FROM chickens WHERE id = {selected_id}")
+
+        conn.commit()
+        
+        print("Chicken record deleted.")
+        return    
+
+
+if __name__ == '__main__':
+    main()
---
+++ Week-4/src/functions.py
@@ -0,0 +1,129 @@
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+#Used to store repitive validation functions
+ 
+
+
+def string_input_validation(prompt):
+     
+    while True:
+
+        user_input=input(prompt)
+        
+        if user_input.replace(" ","").isalpha():  #ensure input is alphabet 
+            return user_input
+        
+        else:
+            print("Invalid input! Please enter alphabet only. No numbers or special characters") 
+
+def integer_input_validation(prompt):
+     
+    while True:
+
+        try:
+            
+            user_input= input(prompt) #user input
+
+            user_input=int(user_input) #ensure input is digit
+
+            if user_input <= 0:
+                print("Entry cannot be 0 or negative please try again.") #error handling fro zero and negative values
+
+            else:
+                return user_input #leaves loop once user input passes validation
+            
+        except:
+            print("Invalid input! Please enter a valid number.")
+
+def user_ID_selection_validation(table_name, prompt):
+    #loop until they select a valid id
+    while True:
+        try:
+            user_input=integer_input_validation(prompt) #ensure they can only enter int
+
+            #selects everything from the chosen table
+            cursor.execute(f"SELECT * FROM {table_name}")
+            valids_id= {row[0] for row in cursor.fetchall()} #store ids 
+
+            if user_input in valids_id: #search through the set for to see if the selection is valid
+                return user_input #if user input matches id then it is returned
+            else:
+                print("Invalid ID! Please select a valid ID from the table.")
+                continue
+
+        except ValueError:
+            print("Invalid input! Please enter a numeric ID.")
+            return
+        
+def confirmation(prompt): # check if the user would like to continue with this option or to go back to the main menu
+
+      while True:
+        confirmation=integer_input_validation(f"{prompt} \n1. Yes or 2. No.\n")
+        
+        if confirmation == 1:
+            break
+
+        elif confirmation == 2:
+            print("Cancelled. Returning to the Main Menu...")
+            return 2
+        
+        else:
+            print("Invalid selection. Please select one of the two options.\n")
+
+def clr_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')#clear terminal
+
+# DB functions 
+
+load_dotenv()
+host_name = os.environ.get("MYSQL_HOST")
+database_name = os.environ.get("MYSQL_DB")
+user_name = os.environ.get("MYSQL_USER")
+user_password = os.environ.get("MYSQL_PASSWORD")
+
+
+def db_connection():
+    print('Opening connection...')
+    try:
+        conn = mysql.connector.connect(
+            host=host_name,
+            database=database_name, 
+            user=user_name,
+            password=user_password
+        )
+        print("Connection established!")
+        cursor = conn.cursor()
+        print("Cursor opened...")
+
+
+        return conn, cursor
+        
+    except Exception as e:
+        print(f"Failed to connect: {e}")
+
+conn,cursor=db_connection()
+
+def close_db_connection(cursor, conn):
+    if cursor:
+        cursor.close()
+    print("Cursor closed.")
+    if conn:
+        conn.close()
+    print("Database connection closed.")
+
+def create_table():
+    tables_sql =f""" CREATE TABLE chickens (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    breed VARCHAR(50),
+    age INT
+);"""
+    cursor.execute(tables_sql)
+
+
+
+
+                
+
